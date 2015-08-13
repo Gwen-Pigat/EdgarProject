bb91888c55483e51448ce7079f4ebc54719bf4f6
Gwen-Pigat <pixofheaven@gmail.com>
2015-08-13 13:17:14 +0200

    Ajout du footer du site

---
+++ include/footer.php
@@ -0,0 +1,21 @@
+<footer id="footer" class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+      <div class="list_first">
+        <ul>
+          <a href="index.php?faq"><li>FAQ</li></a>
+          <a href="index.php?devenir_edgar"><li>DEVENIR EDGAR</li></a>
+        </ul>
+      </div>
+      <div class="list_footer">
+        <ul>
+          <!-- <a href=""><li>TEMOIGNAGES</li></a>
+          <a href=""><li>PRESSE</li></a> -->
+          <a href="index.php?mentions_legales"><li>MENTIONS LEGALES</li></a>
+          <a href="index.php?conditions_generales"><li>CGV / CGU</li></a>
+        </ul>
+      </div>
+      <div class="socials">
+            <h3><strong>Nous suivre</strong></h5>
+            <a target="_blank" href="https://www.facebook.com/edgarclub"><i class="fa fa-facebook-square fa-3x"></i></a>
+            <a target="_blank" href="https://twitter.com/edgar_club"><i class="fa fa-twitter-square fa-3x"></i></a>
+      </div>
+</footer>
