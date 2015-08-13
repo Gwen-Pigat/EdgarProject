d8a86580b65367a92fa126822baa6f0e57759bd3
Gwen-Pigat <pixofheaven@gmail.com>
2015-08-13 13:14:46 +0200

    Explication du paiement : display none d'origine, ne peut apparare que si l'utilisateur à appuer sur le switcher commencer disponible en formulaire d'inscription (exception pour les requetes à la carte, dans ce cas, l'url va générer un bloc différent étant visible d'origine)

---
+++ include/section_explication.php
@@ -0,0 +1,46 @@
+<?php if (($_GET['offre']) == 'vip_card_requete'){ ?>
+<div class="col-md-12" id="explication_paiement_requete">
+      <p class="description_title">Permettez-moi de vous donner quelques informations préalalables <br> avant de finaliser votre inscription.</p>
+
+      <div class="bloc_1 col-md-4 col-md-offset-2">
+      <p class="description_question">- COMMENT S'EFFECTUERA LE PAIEMENT ? -</p>
+      <p class="description">Avant que le paiement s'effectue nous entrerons en contact avec vous afin de déterminer le prix des services requis.<span class="savoir_plus"><br> +En savoir plus</span><span class="hide_section">Les prix des requêtes sont évidemment très variables et seront déterminés par le biais d'un entretien téléphonique avec notre équipe<br><br>
+      Si des services supplémentaires sont par la suite sollicités, les frais seront avancés par Edgar Club et vous seront facturés chaque fin de semaine, accompagnés de leurs justificatifs. 
+      <br><br>
+      Parce que nous sommes dans une relation de confiance, le premier paiement ne sera débité qu'après la première intervention de votre Edgar.</span></p>
+      </div>
+
+      <div class="bloc_2 col-md-4">
+      <p class="description_question">- QUE SE PASSE-T-IL APRES LE PAIEMENT ? -</p>
+      <p class="description"> Dès votre paiement enregistré,<span class="savoir_plus"><br> +En savoir plus</span><span class="hide_section"> l'équipe d'Edgar Club vous contactera pour fixer la date d'une première rencontre à votre domicile afin d'évaluer précisement vos besoins, vous proposer notre meilleur service, et vous présenter celui qui sera à compter de ce jour, votre majordome personnel.</span></p>
+      </div>
+      <br>
+
+      <p class="description_mention col-md-12"><em>Si vous avez la moindre question, l'équipe d'Edgar Club se tient à votre disposition au 09.66.85.10.21 ou par mail à l'adresse hello@edgarclub.com</em></p>
+</div>
+<?php } else{ ?>
+
+
+<div class="col-md-12" id="explication_paiement">
+      <h1>Edgar, pour vous servir</h1>
+      <p>Je suis ravi de votre confiance.</p>
+
+      <p class="description_title">Permettez-moi de vous donner quelques informations préalalables <br> avant de finaliser votre inscription.</p>
+
+      <div class="bloc_1 col-md-4 col-md-offset-2">
+      <p class="description_question">- COMMENT S'EFFECTUE LE PAIEMENT ? -</p>
+      <p class="description">Le paiement s'effectue par prélèvement bancaire de manière automatique, toutes les fins de semaine <span class="savoir_plus"><br> +En savoir plus</span><span class="hide_section">en fonction du montant de la formule choisie. En cliquant sur le bouton "Inscription", il va vous être demandé de renseigner vos informations bancaires sur un site sécurisé de paiement (Stripe, utilisant un algorithme de chiffrement, niveau 1 de certification PCI, offrant le niveau le plus élévé de sécurité, certifié par VISA et MasterCard).<br><br>
+      Si des services supplémentaires (Edgar à la carte, frais de pressing, de courses, etc...) ont été sollicités, les frais sont avancés par Edgar Club et vous seront facturés chaque fin de semaine et les justificatifs vous seront communiqués. 
+      <br><br>
+      Parce que nous sommes dans une relation de confiance, le premier paiement ne sera débité qu'après la première intervention de votre Edgar.</span></p>
+      </div>
+
+      <div class="bloc_2 col-md-4">
+      <p class="description_question">- QUE SE PASSE-T-IL APRES LE PAIEMENT ? -</p>
+      <p class="description"> Dès votre inscription enregistrée,<span class="savoir_plus"><br> +En savoir plus</span><span class="hide_section"> l'équipe d'Edgar Club vous contactera pour fixer la date d'une première rencontre à votre domicile afin d'évaluer précisement vos besoins, vous proposer notre meilleur service, et vous présenter celui qui sera à compter de ce jour, votre majordome personnel.</span></p>
+      </div>
+      <p class="description_mention col-md-12"><em>Si vous avez la moindre question, l'équipe d'Edgar Club se tient à votre disposition au 09.66.85.10.21 ou par mail à l'adresse hello@edgarclub.com</em></p>
+
+</div>
+
+<?php } ?>
