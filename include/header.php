e0ee2896cec434add611a8b34b1cee6047a9c7f7
Gwen-Pigat <pixofheaven@gmail.com>
2015-08-13 12:59:15 +0200

    Disable le zoom en format mobile pour aspect plus natif

--- include/header.php
+++ include/header.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css" type="text/css" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1 user-scalable=no"/>
     <script type="text/javascript" src="js/jquery.js"></script>	
     <script type="text/javascript" src="js/checkout.js"></script>
     <script type="text/javascript" src="js/functions.js"></script>
