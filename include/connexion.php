5508742ad8ee9dc29120a35a99e82c686d4d046e
Gwen-Pigat <pixofheaven@gmail.com>
2015-08-14 10:22:06 +0200

    Reset info données locales

--- include/connexion.php
+++ include/connexion.php
@@ -1,6 +1,6 @@
 <?php 
 
-$link = new mysqli("edgarclutjgwen.mysql.db","edgarclutjgwen","Edgarclub01","edgarclutjgwen")or die("Erreur connexion"); 
+$link = new mysqli("localhost","root","motdepasselocalhostgwen","Edgar")or die("Erreur connexion");
 
 $query_cookie = $link->query("SELECT * FROM Inscription WHERE prenom='$_COOKIE[Prenom]'");
 $row_cookie = $query_cookie->fetch_object();
