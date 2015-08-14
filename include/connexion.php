7c81ee3ae9ed066e48c96abe0b4805c9ebf2165b
Gwen-Pigat <pixofheaven@gmail.com>
2015-08-14 09:43:05 +0200

    Ajout d'un query qui gère les cookie

--- include/connexion.php
+++ include/connexion.php
@@ -2,4 +2,7 @@
 
 $link = new mysqli("localhost","root","motdepasselocalhostgwen","Edgar")or die("Erreur connexion");
 
+$query_cookie = $link->query("SELECT * FROM Inscription WHERE prenom='$_COOKIE[Prenom]'");
+$row_cookie = $query_cookie->fetch_object();
+
 ?>
