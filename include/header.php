2f7aad280e00e1562923af50356cf53f6c3c27e4
Gwen-Pigat <pixofheaven@gmail.com>
2015-08-25 09:25:41 +0200

    Modifications de la meta content-type vers meta HTML5 -> meta charset + ajout meta description avec variable

--- include/header.php
+++ include/header.php
@@ -1,9 +1,10 @@
 <head>
 
-    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-	<meta name="author" content="SemiColonWeb" />
+    <meta charset="utf-8" />
+	<meta name="description" content="<?php echo $description; ?>" />
 	<link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" type="text/css" />
     <link rel="stylesheet" href="css/style.css" type="text/css" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1 user-scalable=no"/>
