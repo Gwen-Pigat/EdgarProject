10e6d9a7bb4f7d011c6f4739b935ae6b5c99727d
Gwen-Pigat <pixofheaven@gmail.com>
2015-08-25 09:26:57 +0200

    Ajout et renseignement de la variable description avant l'include header pour renseigner selon chaque page la meta description

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
 <!DOCTYPE html>
 
 <html lang="fr">
-<?php require "include/connexion.php";
+<?php require "include/connexion.php"; 
 
 if (isset($_GET['inscription'])) {
 
- $title = "Inscription à Edgar"; include "include/header.php"; ?>
+ $description = "Inscription chez Edar, votre majordome personnel";
+ $title = "Inscription à Edgar"; include "include/header.php";
+
+ $query = $link->query("SELECT * FROM Inscription WHERE id=61");
+ $row = $query->fetch_object();
+
+
+    ?>
 
 <!-- Page d'inscription -->
 <body id="head">
@@ -19,14 +26,67 @@ if (isset($_GET['inscription'])) {
 
 <?php } elseif (isset($_GET) && isset($_GET['validation_inscription']) && !empty($_GET['validation_inscription'])) {
 
-    setcookie("Visites", "Oui", time()+2592000);
-
     echo "<script>alert(\"Merci de votre inscription\")</script>";
 
     $title = "Validation inscription"; include "include/header.php";
 
     $query = $link->query("SELECT * FROM Inscription WHERE id_crypt='$_GET[validation_inscription]'");
     $row = $query->fetch_object();
+
+    switch ($row->formule) {
+    case 'home':
+      $dossier = "/var/www/html/PHP/Edgar/profilClient/Formule/Home/$row->id - $row->prenom - $row->email";
+      $formule = "Home";
+      break;
+    case 'sweet_home':
+      $dossier = "/var/www/html/PHP/Edgar/profilClient/Formule/Sweet Home/$row->id - $row->prenom - $row->email";
+      $formule = "Sweet Home";
+      break;
+    case 'sweet_home_plus':
+      $dossier = "/var/www/html/PHP/Edgar/profilClient/Formule/Sweet Home Plus/$row->id - $row->prenom - $row->email";
+      $formule = "Sweet Home Plus";
+      break;
+    case 'vip_card':
+      $dossier = "/var/www/html/PHP/Edgar/profilClient/Formule/A la carte/$row->id - $row->prenom - $row->email";
+      $formule = "A la carte";
+      $request = "\n - Requête: $row->request";
+      break;
+    default:
+      $dossier = "/var/www/html/PHP/Edgar/profilClient/Erreur/$row->prenom - $row->email";
+      break;
+    }
+
+
+    // Insertion d'un fichier client permettant de voir les inscrits + création dossier perso du client lors de son inscription
+
+    mkdir("$dossier", 0777, true);
+    touch("$dossier/Informations.txt", 0777, true);
+
+    $open = file_get_contents("$dossier/Informations.txt");
+    $open = file_get_contents("profilClient/Main.txt");
+
+    $open .= "Identifiant $row->id\n
+    - Offre choisie: $row->formule\n
+    - Nom: $row->nom\n
+    - Prenom: $row->prenom\n
+    - Email: $row->email\n
+    - téléphone: $row->telephone\n
+    - Ville: $row->city\n
+    - Code postal: $row->zip_code\n
+    - Adresse: $row->adresse\n
+    - Date d'inscription: $row->date\n
+    - ID crypté: $row->id_crypt\n
+    - ID crypté 1: $row->id_crypt_1\n
+    - IP : $_SERVER[REMOTE_ADDR] $request\n\n\n\n\n\n";
+
+    file_put_contents("$dossier/Informations.txt", $open);
+    file_put_contents("/var/www/html/PHP/Edgar/profilClient/Main.txt", $open);
+
+    chmod("/var/www/html/PHP/Edgar/profilClient", 0777);
+    chmod("/var/www/html/PHP/Edgar/profilClient/Formule", 0777);
+    chmod("/var/www/html/PHP/Edgar/profilClient/Formule/$row->formule", 0777);
+    chmod("$dossier", 0777);
+    chmod("$dossier/Informations.txt", 0777);
 
     ucfirst($row->prenom);
     setcookie("Prenom", "$row->prenom", time()+2592000);
@@ -52,6 +112,9 @@ if (isset($_GET['inscription'])) {
           $requete = "";
           break;
       }
+
+
+      // Envoi des mails
     
       require "PHPMailer/class.phpmailer.php";
 
@@ -67,7 +130,7 @@ if (isset($_GET['inscription'])) {
         <div style='background-color: white; padding: 4%'>
           <h1 style='color: #ff0000; text-align: center; margin: 0% ; font-family: Verdana; font-size: 4vw'>Edgar Club</h1>
           <h2 style='color: #000; text-align: center; margin: 0% ; font-family: Verdana; font-size: 2vw'>Votre inscription</h2>
-          <p style='text-align: justify ;color: #6D6E72; Margin-top: 7%; font-family: -webkit-body; font-size: 1.3vw'><strong>Merci à vous d'avoir rejoint votre communauté, vous trouverez ci dessous les informations saisies lors de votre inscription :</strong></p>
+          <p style='text-align: justify ;color: #6D6E72; Margin-top: 7%; font-family: -webkit-body; font-size: 1.3vw'><strong>Merci à vous d'avoir rejoint notre communauté, vous trouverez ci dessous les informations saisies lors de votre inscription :</strong></p>
           <div style='text-align: justify; padding: 2%; Margin-top: 4%; color: #6d6e72; font-size: 1.3vw'>- Nom : $row->nom <br />
           - Prénom : $row->prenom <br />
           - Email : $row->email <br />
@@ -120,9 +183,9 @@ if (isset($_GET['inscription'])) {
 
       $txt='This email was sent in HTML format. Please make sure your preferences allow you to view HTML emails.'; 
       $mail->AltBody = $txt; 
-
       $mail->AddAddress("pixofheaven@gmail.com");
       $mail->Send();
+      
 ?>
 
 <a href="index.php">
@@ -145,6 +208,8 @@ if (isset($_GET['inscription'])) {
 
 elseif (isset($_GET) && isset($_GET['faq'])) {
 
+
+ $description = "EdgarCLub, la foire aux questions. Des intérogations sur le fonctionnement de notre site ? N'attendez plus, trouvez vos réponses dès maintenant à l'iade de notre FAQ";
  $title = "FAQ"; include "include/header.php"; ?>
 <!-- Page d'inscription -->
 <body id="head">
@@ -163,6 +228,7 @@ elseif (isset($_GET) && isset($_GET['faq'])) {
 
 elseif (isset($_GET) && isset($_GET['devenir_edgar'])) {
 
+ $description = "Edgar Club | Devenir Edgar. Venez rejoindre l'aventure et devenez une Edgar au sein de notre entreprise !";
  $title = "Devenir Edgar"; include "include/header.php"; ?>
 <!-- Page d'inscription -->
 <body id="head">
@@ -182,6 +248,8 @@ elseif (isset($_GET) && isset($_GET['devenir_edgar'])) {
 
 elseif (isset($_GET) && isset($_GET['mentions_legales'])) {
 
+
+ $description = "Edgar Club | Mentions légales";
  $title = "Mentions légales"; include "include/header.php"; ?>
 <!-- Page d'inscription -->
 <body id="head">
@@ -202,6 +270,7 @@ elseif (isset($_GET) && isset($_GET['mentions_legales'])) {
 
 elseif (isset($_GET) && isset($_GET['conditions_generales'])) {
 
+ $description = "Edgar Club | CGV. Cette page contient les conditions générales de vente rélatives à EdgarClub"; 
  $title = "Conditions générales de vente"; include "include/header.php"; ?>
 <!-- Page d'inscription -->
 <body id="head">
@@ -221,9 +290,9 @@ elseif (isset($_GET) && isset($_GET['conditions_generales'])) {
 
 elseif (isset($_GET['token_access']) && isset($_GET['identifiant']) && !empty($_GET['token_access']) && !empty($_GET['identifiant'])) {
 
-      if (($_GET['token_access']) ==  "NULL" && ($_GET['identifiant']) == "NULL" || ($_GET['token_access']) ==  "NULL" && ($_GET['identifiant']) == "NULL") {
+      if (($_GET['token_access']) ==  "2ba928ccb63ec1646618ba5a6ba98fb4e143fd0a4b84b15bff6ee5796152495a230e45e3d7e947d9" && ($_GET['identifiant']) == "Guillaume" || ($_GET['token_access']) ==  "2ba928ccb63ec1646618ba5a6ba98fb4e143fd0a4b84b15bff6ee5796152495a230e45e3d7e947d9" && ($_GET['identifiant']) == "Jean-carles") {
 
-      $user = $_GET['identifiant'] ; 
+      $user = $_GET['identifiant'];
       $title = "Bonjour $user"; 
       include "include/header.php";
 
@@ -257,7 +326,6 @@ elseif (isset($_GET['acces_paiement']) && isset($_GET['string']) && !empty($_GET
   $title = "Paiement offre $_GET[offre]"; 
       include "include/header.php"; ?>
 
-<!-- Page Admin -->
 <body id="head">
   
     <section id="section_paiement">
@@ -271,6 +339,8 @@ elseif (isset($_GET['acces_paiement']) && isset($_GET['string']) && !empty($_GET
 
 // Si l'utilisateur quitte au moment de payer, dans ce cas on le supprime de la BDD
 $link->query("DELETE FROM Inscription WHERE paiement='En attente'");
+
+// Si l'utilisateur est supprimé, son cookie également
 $query = $link->query("SELECT * FROM Inscription WHERE prenom='$_COOKIE[Prenom]'");
 $row = $query->fetch_object();
 
@@ -278,10 +348,21 @@ if (empty($row)) {
   setcookie("Prenom","$_COOKIE[Prenom]",time() - 3600);
 }
 
+$query = $link->query("SELECT * FROM card_request");
+$row = $query->fetch_object();
+
+$query = $link->query("SELECT * FROM Inscription WHERE email='$row->email'");
+
+if ($row == 0) {
+  $link->query("DELETE FROM card_request WHERE email!='$row->email'");
+}
+
 ?>
 
 <!-- Page par défault -->
-<?php $title = "Enchanté, je suis Edgar"; include "include/header.php"; ?>
+<?php 
+$description = "Edgar Club | Page d'accueil. Bienvenur chez Edgar CLub !";
+$title = "Enchanté, je suis Edgar"; include "include/header.php"; ?>
 
 <body id="head">
         
